5211d5f53c32e018bdc75d059598e07b2fb0e8f2
PJB0238192 <phillip.boiteau@gmail.com>
2022-02-15 16:40:45 +1030

    Resolved merge conflict

---
+++ week1/first.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#inputs text to terminal
+#Phillip 19/01/22
+
+echo "Hi there!"
+
+exit 22
---
+++ week1/second.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#inputs text to terminal
+#Phillip 19/01/22
+
+echo "Hi there!"
+
+echo "It's good to see you $1!"
+
+exit 0
